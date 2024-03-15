9461a493da04063359a442b41752e69a2922eeff
Roxanne <roxanne.gergaud@gmail.com>
2024-03-15 16:18:17 +0100

    SRV-23 : Rotation de l'image lors de l'affichage

--- app/src/main/java/com/example/smallproject_rge_vta/RestaurantActivity.java
+++ app/src/main/java/com/example/smallproject_rge_vta/RestaurantActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -86,7 +88,13 @@ public class RestaurantActivity extends AppCompatActivity {
                         try (InputStream inputStream = getContentResolver().openInputStream(Uri.parse(uriPath))){
                             // Affichage de l'image si on a son contenu
                             Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-                            imageView.setImageBitmap(bitmap);
+
+                            // Rotation à 90 de l'image
+                            Matrix matrix = new Matrix();
+                            matrix.postRotate(90);
+                            Bitmap rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+
+                            imageView.setImageBitmap(rotatedBitmap);
                         } catch (IOException e) {
                             throw new RuntimeException(e);
                         }
