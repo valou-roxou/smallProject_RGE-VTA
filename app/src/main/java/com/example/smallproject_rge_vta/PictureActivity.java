ef0389227af4b36ed5322e51c220f6df85e6b462
Roxanne <roxanne.gergaud@gmail.com>
2024-03-17 14:10:31 +0100

    SRV-26 : Filtre luminosité

--- app/src/main/java/com/example/smallproject_rge_vta/PictureActivity.java
+++ app/src/main/java/com/example/smallproject_rge_vta/PictureActivity.java
@@ -2,11 +2,16 @@ package com.example.smallproject_rge_vta;
 
 import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Matrix;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -23,11 +28,31 @@ import com.google.android.material.tabs.TabLayout;
 import java.io.IOException;
 import java.util.Date;
 
-public class PictureActivity extends AppCompatActivity {
+public class PictureActivity extends AppCompatActivity implements SensorEventListener {
 
     private ImageView imageView;
 
     private FragmentContainerView fragmentContainerView;
+
+    private SensorManager sensorManager;
+
+    private Sensor lightSensor;
+
+
+    private final float[] desaturateMatrix = {
+            0.33f, 0.59f, 0.11f, 0, 0,  // Rouge
+            0.33f, 0.59f, 0.11f, 0, 0,  // Vert
+            0.33f, 0.59f, 0.11f, 0, 0,  // Bleu
+            0,     0,     0,     1, 0   // Alpha
+    };
+
+    private final float[] saturationMatrix = {
+            1.2f, 0, 0, 0, 0, // Rouge
+            0, 1.2f, 0, 0, 0, // Vert
+            0, 0, 1.2f, 0, 0, // Bleu
+            0, 0,  0  , 1, 0     // Alpha
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +67,9 @@ public class PictureActivity extends AppCompatActivity {
 
         imageView = findViewById(R.id.filter_picture_picture);
         imageView.setDrawingCacheEnabled(true);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
 
         Intent intent = getIntent();
         if(intent != null) {
@@ -89,24 +117,17 @@ public class PictureActivity extends AppCompatActivity {
     }
 
     public void onClickNoFilter(View view) {
+        sensorManager.unregisterListener(this);
         imageView.getDrawable().clearColorFilter();
     }
 
     public void onClickFilter1(View view) {
-        // Black and white filter
-        float[] blackAndWhiteMatrix = {
-                0.33f, 0.59f, 0.11f, 0, 0,  // Rouge
-                0.33f, 0.59f, 0.11f, 0, 0,  // Vert
-                0.33f, 0.59f, 0.11f, 0, 0,  // Bleu
-                0,     0,     0,     1, 0   // Alpha
-        };
-        ColorMatrix colorMatrix = new ColorMatrix();
-        colorMatrix.set(blackAndWhiteMatrix);
-        ColorMatrixColorFilter colorFilter = new ColorMatrixColorFilter(colorMatrix);
-        imageView.getDrawable().setColorFilter(colorFilter);
+        sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     public void onClickFilter2(View view) {
+        sensorManager.unregisterListener(this);
+
         // Negative filter
         float[] negativeMatrix = {
                 -1, 0, 0, 0, 255, // Rouge
@@ -119,6 +140,30 @@ public class PictureActivity extends AppCompatActivity {
         ColorMatrixColorFilter colorFilter = new ColorMatrixColorFilter(colorMatrix);
         imageView.getDrawable().setColorFilter(colorFilter);
     }
+
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+        float lightning = event.values[0];
+
+        float[] matriceRes = interpolateMatrices(desaturateMatrix, saturationMatrix, lightning);
+
+        ColorMatrix colorMatrix = new ColorMatrix();
+        colorMatrix.set(matriceRes);
+        ColorMatrixColorFilter colorFilter = new ColorMatrixColorFilter(colorMatrix);
+        imageView.getDrawable().setColorFilter(colorFilter);
+    }
+
+    public static float[] interpolateMatrices(float[] matrix1, float[] matrix2, float y) {
+        float[] interpolatedMatrix = new float[matrix1.length];
+        float ratio = y / 1000f;
+        for (int i = 0; i < matrix1.length; i++) {
+            interpolatedMatrix[i] = matrix1[i] + (matrix2[i] - matrix1[i]) * ratio;
+        }
+        return interpolatedMatrix;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
 
     private final TabLayout.OnTabSelectedListener tabListener = new TabLayout.OnTabSelectedListener() {
         @Override
